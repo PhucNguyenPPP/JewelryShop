2b1b03573040bf7b7fec602c9841cb4493a8e6ce
nghthtam <thanhtamthd3@gmail.com>
2024-06-17 22:29:54 +0700

    [Add Promotion Program and Promotion Code]

---
+++ BLL/Services/PromotionProgramService.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLL.Interfaces;
+using BOL;
+using DTO;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Repositories.Interfaces;
+
+namespace BLL.Services
+{
+    public class PromotionProgramService : IPromotionProgramService
+    {
+        private readonly IPromotionProgramRepository _promotionProgramRepository;
+        public PromotionProgramService(IPromotionProgramRepository promotionProgramRepository)
+        {
+            _promotionProgramRepository = promotionProgramRepository;
+        }
+        public List<PromotionProgram> GetPromotionProgramList()
+        {
+            return _promotionProgramRepository.GetPromotionProgramList().ToList();
+        }
+
+        public List<PromotionProgram> SearchPromotionProgram(string searchValue)
+        {
+            List<PromotionProgram> promotionProgramsList = _promotionProgramRepository.GetPromotionProgramList()
+                .Where(c => c.PromotionProgramName.ToLower().Contains(searchValue)).ToList();
+            return promotionProgramsList;
+        }
+
+        public bool DeletePromotionProgram(string promotionProgramId)
+        {
+            PromotionProgram? promotionProgram = _promotionProgramRepository.GetById(Guid.Parse(promotionProgramId));
+            if (promotionProgram == null)
+            {
+                return false;
+            }
+            promotionProgram.Status = false;
+            _promotionProgramRepository.UpdatePromotionProgram(promotionProgram);
+            bool result = _promotionProgramRepository.SaveChange();
+            return result;
+        }
+
+        public bool AddPromotionProgram(PromotionProgramDTO promotionProgramDTO)
+        {
+            DateTime.TryParse(promotionProgramDTO.ExpiredDate, out DateTime expiredDate);
+            Guid promotionProgramID = Guid.NewGuid();
+            PromotionProgram promotionProgram = new PromotionProgram()
+            {
+                PromotionProgramId = promotionProgramID,
+                PromotionProgramName = promotionProgramDTO.PromotionProgramName,
+                CreatedDate = DateTime.Now,
+                ExpiredDate = expiredDate,
+                Status = true
+            };
+            _promotionProgramRepository.AddPromotionProgram(promotionProgram);
+            bool result = _promotionProgramRepository.SaveChange();
+            return result;
+        }
+
+        public bool UpdatePromotionProgram(PromotionProgramDTO promotionProgramDTO)
+        {
+            Guid.TryParse(promotionProgramDTO.PromotionProgramId, out Guid promotionProgramID);
+            DateTime.TryParse(promotionProgramDTO.ExpiredDate, out DateTime expiredDate);
+            DateTime.TryParse(promotionProgramDTO.CreatedDate, out DateTime createdDate);
+
+            PromotionProgram promotionProgram = new PromotionProgram()
+            {
+                PromotionProgramId = promotionProgramID,
+                PromotionProgramName= promotionProgramDTO.PromotionProgramName,
+                CreatedDate= createdDate,
+                ExpiredDate= expiredDate,
+            };
+            _promotionProgramRepository.UpdatePromotionProgram(promotionProgram);
+            bool result = _promotionProgramRepository.SaveChange();
+            return result;
+        }
+    }
+}
---
+++ DTO/PromotionCodeDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class PromotionCodeDTO
+    {
+        public string? PromotionCodeId { get; set; }
+
+        public string? PromotionCodeName { get; set; }
+
+        public decimal? DiscountPercentage { get; set; }
+
+        public string? PromotionProgramId { get; set; }
+    }
+}
