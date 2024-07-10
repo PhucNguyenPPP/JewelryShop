cf7743ba62cf3a88bfa02373601f02c540704ed1
nghthtam <thanhtamthd3@gmail.com>
2024-07-10 09:20:03 +0700

    [Add]CounterDTO

---
+++ DTO/CounterDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+	public class CounterDTO
+	{
+		public string? CounterId { get; set; }
+
+		[Required(ErrorMessage = "Please input counter name!")]
+		[RegularExpression("^[a-zA-Z0-9 ]+$", ErrorMessage = "Counter name can not include special character!")]
+		public string? CounterName { get; set; }
+	}
+}
